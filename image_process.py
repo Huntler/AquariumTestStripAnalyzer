4624b9f15b2616dce546936bb998fe0bc464b5e6
Jonas Leuchtenberger <jonas.leuchtenberger@rwth-aachen.de>
2023-09-06 02:24:08 +0200

    Add strip extraction

---
+++ image_process.py
@@ -0,0 +1,177 @@
+from typing import Tuple
+import cv2
+import numpy as np
+import os
+
+
+def pre_process(image: np.array) -> np.array:
+    # Copy the image to avoid changes
+    image = image.copy()
+
+    # Smooth the image
+    blur = cv2.GaussianBlur(image, (15, 15), 3)
+
+    # Morph: Close to Open
+    kernel = np.ones((12, 12), np.uint8)
+    morphed = cv2.morphologyEx(
+        blur,
+        cv2.MORPH_CLOSE,
+        kernel,
+        anchor=(-1, -1),
+        iterations=2,
+        borderType=cv2.BORDER_CONSTANT,
+    )
+    morphed = cv2.morphologyEx(morphed, cv2.MORPH_OPEN, kernel)
+
+    # Morph: Dialate
+    dialate_param = 5
+    kernel = np.ones((dialate_param, dialate_param), np.uint8)
+    noise_reduced = cv2.morphologyEx(morphed, cv2.MORPH_DILATE, kernel)
+
+    return noise_reduced
+
+
+def detect_edges(image: np.array) -> Tuple[np.array, int]:
+    # Edge detection
+    canny = cv2.Canny(image, 50, 100, apertureSize=3, L2gradient=True)
+
+    # Post-process edges
+    # Morph: Dialate
+    dialate_param = 50
+    iterations = 4
+    kernel = np.ones((dialate_param, dialate_param), np.uint8)
+    noise_reduced = cv2.morphologyEx(
+        canny, cv2.MORPH_DILATE, kernel, iterations=iterations
+    )
+
+    return noise_reduced, (dialate_param * iterations) // 2
+
+
+def locate_strip(image: np.array, edges: np.array, padding: int = 0) -> np.array:
+    # Get contours of the image (given edges)
+    contours = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    contours = contours[0] if len(contours) == 2 else contours[1]
+
+    # Find the contours covering the largest area
+    area = 0
+    for c in contours:
+        contour_area = cv2.contourArea(c)
+        if area < contour_area:
+            area = contour_area
+            big_contour = c
+
+    # Rotate rectangle matching the contour
+    rot_rect = cv2.minAreaRect(big_contour)
+    box = cv2.boxPoints(rot_rect)
+    box = np.intp(box)
+
+    # Clip the box
+    box[:, 0] = np.clip(box[:, 0], 0, image.shape[0])
+    box[:, 1] = np.clip(box[:, 1], 0, image.shape[1])
+
+    # Extract the rectangle and perform perspective transformation
+    min_x = np.min(box[:, 0])
+    max_x = np.max(box[:, 0])
+    min_y = np.min(box[:, 1])
+    max_y = np.max(box[:, 1])
+    extracted_region = image[min_y:max_y, min_x:max_x]
+
+    # Rotate the extracted region to align it with the horizontal axis
+    angle = rot_rect[2] if rot_rect[2] > -45 else rot_rect[2] + 90
+    rotated_extracted_region = cv2.warpAffine(
+        extracted_region,
+        cv2.getRotationMatrix2D(
+            (extracted_region.shape[1] / 2, extracted_region.shape[0] / 2), angle, 1
+        ),
+        (extracted_region.shape[1], extracted_region.shape[0]),
+    )
+
+    # Apply the padding if given
+    if padding > 0:
+        return rotated_extracted_region[padding:-padding, padding:-padding]
+
+    return rotated_extracted_region
+
+
+def strip_pipeline(image_path: str, save_intermediates: bool = False) -> np.array:
+    image_name = os.path.splitext(os.path.basename(image_path))[0]
+    output_dir = f"./data/strips/{image_name}"
+    save = (
+        lambda img, pre: save_image(image_name, img, pre, output_dir)
+        if save_intermediates
+        else 0
+    )
+
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Transform to gray scale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Run the pipeline
+    # 1. Pre-process the image
+    pre_processed = pre_process(gray)
+    save(pre_processed, "pre")
+
+    # 2. Find edges
+    edges, offset = detect_edges(pre_processed)
+    save(edges, "edges")
+
+    # 3. Extract the strip only
+    # add gaussian kernel, morph:open, and morph:dialate to the offset (7 + 12 + 5 = 24)
+    offset += 24
+    extracted_strip = locate_strip(image, edges, padding=offset)
+    save(extracted_strip, "extracted")
+
+    # 4. Analyze the white color to perform a white balance
+    # TODO: white balance
+
+    # 5. extract the 9 color patches
+    # TODO: detect colored patches
+
+    return extracted_strip
+
+
+def save_image(
+    file_name: str,
+    image: np.array,
+    prefix: str = "",
+    folder_name: str = "",
+    create_subfolder: bool = True,
+):
+    # Create the file name with prefix and file extension
+    file_name = file_name if prefix == "" else f"{prefix}_{file_name}.jpg"
+
+    # Create the full path and needed directories
+    if not os.path.exists(folder_name) and create_subfolder:
+        os.mkdir(folder_name)
+
+    # Store the image
+    full_path = os.path.join(folder_name, file_name)
+    cv2.imwrite(full_path, image)
+
+
+def improve_contrast(image: np.array) -> np.array:
+    lab = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
+    l_channel, a, b = cv2.split(lab)
+    # Applying CLAHE to L-channel
+    # feel free to try different values for the limit and grid size:
+    clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8, 8))
+    cl = clahe.apply(l_channel)
+
+    # merge the CLAHE enhanced L-channel with the a and b channel
+    limg = cv2.merge((cl, a, b))
+
+    # Converting image from LAB Color model to BGR color spcae
+    enhanced_img = cv2.cvtColor(limg, cv2.COLOR_LAB2BGR)
+    return enhanced_img
+
+
+if __name__ == "__main__":
+    output_dir = "./data/strips/"
+
+    # Find the strip and remove the background
+    image_path = "./data/raw_input/"
+    for file in os.listdir(image_path):
+        file_path = image_path + file
+        strip_pipeline(file_path, save_intermediates=True)
